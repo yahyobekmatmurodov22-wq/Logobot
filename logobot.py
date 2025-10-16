6f2d6ef8afe0c314b129ff3a48c639fc5674bb71
yahyobekmatmurodov22-wq <yahyobekmatmurodov22@gmail.com>
2025-10-16 06:59:24 +0500

    Update logobot.py

--- logobot.py
+++ logobot.py
@@ -54,4 +54,17 @@ def logo_yarat(message):
         bot.reply_to(message, f"Xatolik yuz berdi: {e}")
 
 print("🤖 Logo bot ishga tushdi!")
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+threading.Thread(target=run).start()
 bot.polling(none_stop=True)
